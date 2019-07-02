daa463328bd2379507b51e7ff61727c916a6f3e6
renjinbo <ren.jinbo@navercorp.com>
2019-07-02 14:37:36 +0800

    Initial Commit

---
+++ JBCut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JBCut
+//
+//  Created by 任金波 on 2019/7/2.
+//  Copyright © 2019 任金波. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
